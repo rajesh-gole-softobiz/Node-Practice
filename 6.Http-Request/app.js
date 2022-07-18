fd71caa1300dcb54b7e916990c14ab9396bdf969
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-07-18 17:17:52 +0530

    http request using header (postman)

--- 6.Http-Request/app.js
+++ 6.Http-Request/app.js
@@ -2,20 +2,28 @@ const express = require('express');
 const app=express();
 
 
+// app.get('/',(req,res)=>{
+//     // http request with query parameter localhost:3000/?id=101
+//     const id=req.query.id;
+//     res.send(`<h1>Student Id: ${id}</h1>`);
+
+// });
+
+// app.get('/userId/:id/userAge/:age',(req,res)=>{
+//     // http request with route parameter localhost:3000/userId/101/userAge/25
+//    const id = req.params.id;
+//    const age = req.params.age;
+
+//    res.send(`<h1>Student id is : ${id}, and age is: ${age}</h1>`);
+// });
+
+// http request using header (postman)
 app.get('/',(req,res)=>{
-    // http request with query parameter localhost:3000/?id=101
-    const id=req.query.id;
-    res.send(`<h1>Student Id: ${id}</h1>`);
+    const id = req.header('id');
+    const name =req.header('name');
 
-});
-
-app.get('/userId/:id/userAge/:age',(req,res)=>{
-    // http request with route parameter localhost:3000/userId/101/userAge/25
-   const id = req.params.id;
-   const age = req.params.age;
-
-   res.send(`<h1>Student id is : ${id}, and age is: ${age}</h1>`);
-});
+    res.send(`<h1>Student id is ${id} and name is ${name}</h1>`);
+})
 
 app.use((req,res)=>{
     res.send(`<h1>404 page not found!</h1>`)
