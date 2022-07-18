a91442d5aa118ae358bea0dc8069134adb5d0153
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-07-18 17:11:01 +0530

    http request with route parameter

--- 6.Http-Request/app.js
+++ 6.Http-Request/app.js
@@ -6,6 +6,15 @@ app.get('/',(req,res)=>{
     // http request with query parameter localhost:3000/?id=101
     const id=req.query.id;
     res.send(`<h1>Student Id: ${id}</h1>`);
+
+});
+
+app.get('/userId/:id/userAge/:age',(req,res)=>{
+    // http request with route parameter localhost:3000/userId/101/userAge/25
+   const id = req.params.id;
+   const age = req.params.age;
+
+   res.send(`<h1>Student id is : ${id}, and age is: ${age}</h1>`);
 });
 
 app.use((req,res)=>{
