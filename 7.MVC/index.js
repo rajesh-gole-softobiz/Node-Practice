159c6b608e2621ae87ff00e4389ae791b37c9877
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-07-25 11:01:13 +0530

    Creating form and get data by posting

--- 7.MVC/index.js
+++ 7.MVC/index.js
@@ -2,9 +2,33 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// To access body and find data
+app.use(express.urlencoded({extended:true}))
+
+// Creating Form
+const htmlForm = `
+<form method="POST" actions="/users">
+    <input type="text" name="name" placeholder="Enter Name" />
+    <input type="number" name="age" placeholder="Enter Age" />
+    <button type="submit">Save User</button>
+</form>
+`
+
+
 app.get('/users',(req,res)=>{
-    res.send('Hello Users')
+    res.send(htmlForm)
 })
+
+app.post('/users',(req,res)=>{
+    
+})
+
+app.use((req,res,next)=>{
+    res.status(404).json({
+        message : "Resource Not Found"
+    })
+})
+
 app.listen(PORT,()=>{
     console.log(`Server started at ${PORT}`);
 })
