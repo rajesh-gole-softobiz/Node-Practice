c8703d3cd5544f335151b4a9c6a526daca701b31
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-07-25 11:41:29 +0530

    Uses of MVC architecture

--- 7.MVC/index.js
+++ 7.MVC/index.js
@@ -2,48 +2,54 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const userRouter = require('./routes/users.route');
+
 // To access body and find data
 app.use(express.urlencoded({extended:true}))
+//To use user router
+app.use(userRouter);
 
-//
-const users = [
-    {
-        name: "Rajesh Gole",
-        age : 26
-    },
-    {
-        name: "abc xyz",
-        age : 31
-    },
-]
+// Models Part
 
-// Creating Form
-const htmlForm = `
-<form method="POST" actions="/users">
-    <input type="text" name="name" placeholder="Enter Name" />
-    <input type="number" name="age" placeholder="Enter Age" />
-    <button type="submit">Save User</button>
-</form>
-`
+// const users = [
+//     {
+//         name: "Rajesh Gole",
+//         age : 26
+//     },
+//     {
+//         name: "abc xyz",
+//         age : 31
+//     },
+// ]
 
+// Creating Form - Views Part
+// const htmlForm = `
+// <form method="POST" actions="/users">
+//     <input type="text" name="name" placeholder="Enter Name" />
+//     <input type="number" name="age" placeholder="Enter Age" />
+//     <button type="submit">Save User</button>
+// </form>
+// `
 
-app.get('/users',(req,res)=>{
-    res.send(htmlForm)
-})
+//Routes & Controllers Part
 
-app.post('/users',(req,res)=>{
-    const name = req.body.name;
-    const age = Number(req.body.age);
-    const user = {
-        name,
-        age
-    }
-    users.push(user)
-    res.status(201).json({
-        success: true,
-        users
-    })
-})
+// app.get('/users',(req,res)=>{
+//     res.send(htmlForm)
+// })
+
+// app.post('/users',(req,res)=>{
+//     const name = req.body.name;
+//     const age = Number(req.body.age);
+//     const user = {
+//         name,
+//         age
+//     }
+//     users.push(user)
+//     res.status(201).json({
+//         success: true,
+//         users
+//     })
+// })
 
 app.use((req,res,next)=>{
     res.status(404).json({
---
+++ 7.MVC/models/users.model.js
@@ -0,0 +1,12 @@
+const users = [
+    {
+        name: "Rajesh Gole",
+        age : 26
+    },
+    {
+        name: "abc xyz",
+        age : 31
+    },
+]
+
+module.exports = users;
